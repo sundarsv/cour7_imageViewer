d04534dacfa581e59b55046a307a0c7943beba9b
Srinivasan <sundarsv@gmail.com>
2018-11-12 06:00:51 +0530

    Setting up controller in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Controller from '../src/screens/Controller'
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
+    <Controller />,
     document.getElementById('root')
 );
