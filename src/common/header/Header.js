8370db31b40f539a9fd710348d3316794575e994
vijay-mamoria <theprofessionalcoder@gmail.com>
2018-11-18 22:55:28 +0530

    1. Added Profile Icon 2. Added Menu Item

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,15 +1,48 @@
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
 import Input from '@material-ui/core/Input';
 import Search from '@material-ui/icons/Search';
 import React, { Component } from 'react';
 import './Header.css';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 class Header extends Component {
 
     constructor() {
         super();
         this.state = {
-            loggedIn: sessionStorage.getItem("access-token") == null ? false : true
+            loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
+            userData: "",
+            displayMenu: "disp-none"
         }
+    }
+
+    //Using Fetch with async and await to get json data
+    async componentDidMount() {
+        if (this.state.loggedIn) {
+            const response = await fetch(`https://api.instagram.com/v1/users/self/?access_token=9204272757.f8594e7.25756c2b57804b6b8b1cb08b48e45566`);
+            const json = await response.json();
+            this.setState({ userData: json.data });
+        }
+    }
+
+    onProfileIconClickHandler = () => {
+        this.setState({ displayMenu: "disp-block" });
+    }
+
+    onMyAccountClickHandler = () => {
+        this.props.history.push("/profile");
+    }
+
+    /**
+     * Clear Session Storage on Logout.
+     */
+    onLogoutClickHandler = () => {
+        sessionStorage.setItem("access-token", null);
+        sessionStorage.setItem("uuid", null);
+        this.props.history.push("/");
     }
 
     render() {
@@ -19,9 +52,27 @@ class Header extends Component {
                     Image Viewer
                     {
                         this.state.loggedIn &&
-                        <div className="searchBox">
-                            <Search className="searchIcon" />
-                            <Input id="search" type="text" placeholder="Search…" disableUnderline={true}></Input>
+                        <div>
+                            <div className="right">
+                                <div className="searchBox">
+                                    <Search className="searchIcon" />
+                                    <Input id="search" type="text" placeholder="Search…" disableUnderline={true}></Input>
+                                </div>
+                                <div className="profile-picture">
+                                    <IconButton color="default" onClick={this.onProfileIconClickHandler}>
+                                        <Icon><img src={this.state.userData.profile_picture} alt="profile_picture" /></Icon>
+                                    </IconButton>
+                                    <div className={this.state.displayMenu}>
+                                        <MenuItem key="1" value="My Account">
+                                            <ListItemText primary="My Account" onClick={this.onMyAccountClickHandler} />
+                                        </MenuItem>
+                                        <hr />
+                                        <MenuItem key="2" value="Logout">
+                                            <ListItemText primary="Logout" onClick={this.onLogoutClickHandler} />
+                                        </MenuItem>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     }
                 </div>
